61738317739896f9d35d03d4305c0de84583eab7
Ryan Foo <55099779+Jambabba@users.noreply.github.com>
2023-08-03 00:54:38 +0800

    Update reduce.py
    IT FINALLY WORK the SHE-BANG

--- reducer.py
+++ reducer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python3
+#!/usr/bin/python3
 
 from operator import itemgetter
 import sys
@@ -8,24 +8,23 @@ current_count = 0
 word = None
 
 for line in sys.stdin:
-        # remove whitespacees
-        line = line.strip()
-        # parse input from mapper.py
-        word, count = line.split('\t', 1)
-        # convert count to int
-        try:
-           count = int(count)
-        except ValueError:
-           continue 
+    line = line.strip()
+    if '\t' not in line:
+        continue 
+    word, count = line.split('\t', 1)
+    
+    try:
+        count = int(count)
+    except ValueError:
+        continue
 
-
-        if current_word == word:
-           current_count += count
-        else: 
-             if current_word:
-                print(f"{current_word}\t{current_count}")
-             current_count = count
-             current_word = word
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print('%s\t%s' % (current_word, current_count))
+        current_word = word
+        current_count = count
 
 if current_word == word:
-        print(f"{current_word}\t{current_count}")
+    print('%s\t%s' % (current_word, current_count))
